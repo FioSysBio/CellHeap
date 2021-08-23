c7024e07a6269b52c7a3c39c9db73e667191dc63
nessa0389 <74985738+nessa0389@users.noreply.github.com>
2021-08-23 15:38:04 -0300

    Update script_phase4_metacell_mild.R

--- Code/script_phase4_metacell_mild.R
+++ Code/script_phase4_metacell_mild.R
@@ -9,7 +9,7 @@ id = "lung_new"
 ord_id = "lung_new_sorted"
 library(knitr)
 
-#import objeto do Seurat
+#import object of Seurat
 #mild
 mcell_import_scmat_tsv(mat_nm = "all", fn = "/scratch/inova-covd19/vanessa.silva/features_not_infected_mild.tsv", dset_nm = "/scratch/inova-covd19/vanessa.silva/metadata_mild.csv", force = F)
 mat = scdb_mat("all")
